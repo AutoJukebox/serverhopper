6a237bf80c7d1faed7fab2caac0e7d9ce1bcf803
code_gs <collinharris509@icloud.com>
2015-05-22 11:02:37 -0500

    Cleaned up serverside
    - Removed useless if statement
    - Called ConCommand on the player serverside
    - Check text unmodified to prevent '!serverswhatever' from working

--- SvrHprAddon/lua/autorun/server/sv_svrhopper.lua
+++ SvrHprAddon/lua/autorun/server/sv_svrhopper.lua
@@ -1,12 +1,10 @@
-if not SERVER then return end
-
 include("autorun/svrhoppercfg.lua")
 
 hook.Add( "PlayerSay", "ServerHopperCMD", function( ply, text )
 
-	
-
-	    if ( string.sub( text, 1, 8 ) == '!servers' ) then
+	if ( text == '!servers' ) then
     
-       ply:SendLua("LocalPlayer():ConCommand('OpenSvrHpr')")    end
+                ply:ConCommand('OpenSvrHpr')
+
+        end
 end)
